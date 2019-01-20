a5a39417c111acbd930ef57f57af8402bc400d5b
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2019-01-20 10:57:48 -0500

    Updates

---
+++ precache-manifest.3bf90ab109c9e73cb8a9bc7cb935c5c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a349a0d2255ac2ae52bd",
+    "url": "/static/js/main.a349a0d2.chunk.js"
+  },
+  {
+    "revision": "139cd68cf65252727aed",
+    "url": "/static/js/1.139cd68c.chunk.js"
+  },
+  {
+    "revision": "a349a0d2255ac2ae52bd",
+    "url": "/static/css/main.6051c720.chunk.css"
+  },
+  {
+    "revision": "dd99107305c130ae475d1d8d595ad7e0",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d026e81cc38ae55f4676655d7b3af6c.js"
+  "/precache-manifest.3bf90ab109c9e73cb8a9bc7cb935c5c7.js"
 );
 
 workbox.clientsClaim();
